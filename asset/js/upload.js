1728593e7b0103c0d644fe3cee35196b5c0cef57
peterxulove <peter.xzy@gmail.com>
2024-04-06 13:05:09 +0800

    Update upload.js

--- asset/js/upload.js
+++ asset/js/upload.js
@@ -214,10 +214,10 @@ function render_uploaded() {
         var resp = x.resp;
         var name = resp.data.url.split('/').pop();
         
-        const WPRAW = "https://i3.wp.com/telegra.ph"  //使用wordpress.com加速
-		const WPCFRAW = "https://i3.wp.com/imgs.dalao.pro/api"  //使用wordpress.com加速cf,修改后方域名为你的加速域名
+        const WPRAW = "https://image.aibici.org/telegra.ph"  //使用wordpress.com加速
+		const WPCFRAW = "https://image.aibici.org/imgs.dalao.pro/api"  //使用wordpress.com加速cf,修改后方域名为你的加速域名
         const PhRAW = "https://telegra.ph"   //原地址
-        const PROXYURL = ""  //自定义加速域名 默认是使用cloudflare
+        const PROXYURL = "https://image.aibici.org"  //自定义加速域名 默认是使用cloudflare
         const BASE_PROXYURL = PROXYURL ? PROXYURL : BASE_URL;
         
         var url = resp.data.thumb == null ? BASE_PROXYURL + resp.data.url : BASE_PROXYURL + resp.data.thumb.url;
